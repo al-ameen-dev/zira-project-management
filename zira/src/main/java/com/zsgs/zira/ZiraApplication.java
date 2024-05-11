6d372cfb06ee09fed2a32df92800c14650fffe2a
Al Ameen <caalameen123@gmail.com>
2024-05-11 17:49:22 +0530

    Project-Zira initial push

---
+++ zira/src/main/java/com/zsgs/zira/ZiraApplication.java
@@ -0,0 +1,13 @@
+package com.zsgs.zira;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZiraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZiraApplication.class, args);
+	}
+
+}
---
+++ zira/src/test/java/com/zsgs/zira/ZiraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zsgs.zira;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZiraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
